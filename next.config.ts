33d58fef206da066b612f97299dd60984fc84128
JONATHAN SAMUEL MIRANDA ARREBOLO <53109600@sanjuangobierno.onmicrosoft.com>
2026-08-24 21:57:16 -0300

    Optimize build for Render RAM limit

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default withPWA(nextConfig);
